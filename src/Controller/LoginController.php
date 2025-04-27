6bd9c9b5aec8e7f1e25f23de40b32875be0dabae
Adrian Szymczyk <adrianszymczyk2211@gmail.com>
2025-04-27 13:35:46 +0200

    logout functionality

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,4 +22,7 @@ final class LoginController extends AbstractController
             'error' => $error
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout() {}
 }
